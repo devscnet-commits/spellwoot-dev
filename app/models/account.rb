1d00c446a8af8189bbaf64edc4e177da08fb79bd
devscnet-commits <dev.scnet@gmail.com>
2026-08-26 13:55:38 -0300

    fix(account): adiciona has_many :subscriptions (faltava, quebrava criacao de agentes de IA)

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,4 @@
-# == Schema Information
+﻿# == Schema Information
 #
 # Table name: accounts
 #
@@ -99,6 +99,7 @@ class Account < ApplicationRecord
   has_many :webhooks, dependent: :destroy_async
   has_many :whatsapp_channels, dependent: :destroy_async, class_name: '::Channel::Whatsapp'
   has_many :working_hours, dependent: :destroy_async
+  has_many :subscriptions, dependent: :destroy_async
 
   has_one_attached :contacts_export
 
